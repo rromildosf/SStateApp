04955d27f756b8164c1e95c2d37c50d7c75834ca
romildosf <rromildosf@gmail.com>
2020-11-26 02:48:11 -0300

    Added loading on main screen

--- app/src/main/java/com/rsf/challengeapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/rsf/challengeapp/viewmodel/MainViewModel.kt
@@ -3,10 +3,12 @@ package com.rsf.challengeapp.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.rsf.challengeapp.repository.IProductRepository
+import kotlinx.coroutines.delay
 
 class MainViewModel(private val repository: IProductRepository): ViewModel() {
 
     fun getProductList() = liveData {
+        delay(500)
         emit(repository.getProductList())
     }
 }
